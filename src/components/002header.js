19b4b733ddfabefac5751b5e8e9db0ba03b21839
Nilipta <niliptasatapathy@gmail.com>
2019-11-07 23:33:58 +0530

    adding css to project

--- src/components/002header.js
+++ src/components/002header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+//import './specific.css';
+
 const Header = (props) => {
     const {title} = props;
     return (
         <div>
-            <h1> {title} </h1>
+            <h1 style={headingStyle}> {title} </h1>
+            {/* <h1 style={{ color:'green', fontSize:'50px' }}> {title} </h1> */}
         </div>
     );
 };
@@ -16,6 +19,11 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired
+};
+
+const headingStyle = {
+    color: 'red',
+    fontSize: '50px'
 };
 
 export default Header;
