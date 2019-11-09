dfb79dc5db74ef52b54a94a45c2b843f698faa72
Nilipta <niliptasatapathy@gmail.com>
2019-11-09 12:59:31 +0530

    onclick event, font awesome and bind method, coustm function in child

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
-![contact app screenshot](./public/screenshots/contactApp.png)
-# after bootstrapping and including contatcs component and contact componet in contacts component screenshot 2
-![contact app screenshot](./public/screenshots/contactApp2.png)
+![contact app screenshot](./public/screenshots/contactApp3-for-loop-contatcs.png)
 
--- src/components/001contact.js
+++ src/components/001contact.js
@@ -2,12 +2,21 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Contact extends Component {
+    state= {
+
+    };
+
+    onShowClick = (id) => { // arrow function is required for this keyword work in costum function
+        // console.log(argument.target);
+        console.log(id); //if we pass name it will print name from click event
+    }
 
     render() {
         const { name, email, phone } = this.props.contact; //passed from contatcs 
     return (
             <div className="card card-body mb-3">
-                <h4> {name} </h4>
+                <h4> {name} <i onClick={this.onShowClick.bind(this, name)} className="fas fa-sort-down"/> 
+                </h4>
                 <ul className="list-group">
                     <li className="list-group-item">Email: {email} </li>
                     <li className="list-group-item">Phone : {phone} </li>
