3180343592bb96b78f2762cff56c8146eea93e92
Nilipta <niliptasatapathy@gmail.com>
2019-11-11 23:31:01 +0530

    added add contact component & contact form, but value is not inserting in form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import Contacts from './components/003contatcts';
-import Header from './components/002header';
+import Contacts from './components/contact/003contatcts';
+import AddContact from './components/contact/addContact';
+import Header from './components/layout/002header';
 
 import { Provider } from './context';
 
@@ -16,6 +17,7 @@ class App extends Component {
       <div className="App">
         <Header title="Contact Manager"/>
         <div className="container">
+          <AddContact />
           <Contacts/>
         </div>
       </div>
