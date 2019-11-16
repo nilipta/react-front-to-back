3410ac2708f4f3c6b43e204f8c33e202e4a28d72
Nilipta <niliptasatapathy@gmail.com>
2019-11-17 00:05:49 +0530

    Added 404 error page in applcation and added in route in APP.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contacts from './components/contact/003contatcts';
 import AddContact from './components/contact/addContact';
 import Header from './components/layout/002header';
 import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
 
 import { Provider } from './context';
 
@@ -23,6 +24,7 @@ class App extends Component {
                 <Route exact path ="/" component={Contacts} />
                 <Route exact path ="/contact/add" component={AddContact} />
                 <Route exact path ="/about/:id" component={About} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return(
+        <div>
+            <h1 className="display-4">
+                <span className="text-danger"> 404 </span> Page Not found
+            </h1>
+            <p className="lead">
+                Sorry , that page not exist
+            </p>
+        </div>
+    )
+}
